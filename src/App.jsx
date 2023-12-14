739d24def110cb0210c5685bec897efbdfbfd131
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2023-12-14 16:43:07 +0530

    Initial react state management and rendering as per the the auth.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,33 @@ import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import './App.css'
 import authService from './appwrite/auth';
+import {login,logout} from './store/authSlice'
 
 function App() {
 
   const [loading,setLoading]=useState(true)
   const dispatch=useDispatch()
 
+  useEffect(()=>{
+      authService.getCurrentUser()
+      .then((userData)=>{
+          if(userData){
+            dispatch(login({userData}))
+          }
+          else{
+            dispatch(logout())
+          }
+      })
+      .finally(()=>{
+          setLoading(false)
+      })
+
+  },[])
+
+  if(loading){
+    return null
+  }
   
-  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>Hey There!</h1>
